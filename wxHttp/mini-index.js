01c30f6a94b1a40b930b75a4110c83e314ffc0e5
tantian <877566012@qq.com>
2021-01-07 15:04:52 +0800

    second commit

---
+++ wxHttp/mini-index.js
@@ -0,0 +1,59 @@
+
+function request($wx) {
+    //METHODS = ['get', 'post', 'put', 'delete','login','getUserInfo'];
+    //  全局config
+    const config = {};
+    let handler = {
+        get (target, property) {
+            target[property] = (url, params = {}) => {
+                if ( property === 'login'){
+                    return new Promise((resolve, reject) => {
+                        $wx.login({
+                            success: res => {
+                                resolve(res)
+                            },
+                            fail: error => {
+                                reject(error)
+                            }
+                        })
+                    }) }
+                if ( property === 'getUserInfo'){
+                    return new Promise((resolve, reject) => {
+                        $wx.getUserInfo({
+                            success: (res) => {
+
+                                resolve(res)
+                            },
+                            fail: (error) => {
+                                reject(error)
+                            }
+                        })
+
+                    }) }
+                return new Promise((resolve, reject) => {
+                    $wx.request({
+                        url,
+                        method: property.toLocaleUpperCase(),
+                        ...Object.assign(params,config),
+                        success: res => {
+                            if (Number(res.statusCode) !== 200) {
+                                $wx.showToast({ title: '通讯错误，稍后再试', icon: 'none' })
+                                return false
+                            }
+
+                            resolve(res)
+                        },
+                        fail: error => {
+                            reject(error)
+                        }
+                    })
+                })
+            }
+            return target[property]
+        }
+    }
+
+    return new Proxy({}, handler)
+}
+
+module.exports.$http = request
